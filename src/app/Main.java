e532bcc0e0b5dcc516bc3f29b7e90935e8c35731
undefinedDen <tunevdenis123@gmail.com>
2024-12-22 22:24:01 +0200

    initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,16 @@
+package app;
+
+import java.util.Scanner;
+public class Main {
+    static int sumNegative = 0;
+    static int paired = 0;
+    static int notPaired = 0;
+    static int min;
+    static int max;
+    static double sumArr = 0;
+    static double average = 0;
+    static int count = 0;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
